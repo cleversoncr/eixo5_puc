f19a0c026c5b37d98ff15947bfad12a45c2f7784
Cleverson Rocha <cleverson.rocha@minu.co>
2022-03-24 23:53:49 -0300

    Teste com uma unica rota

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-// const connection = require('./database/database');
-// const Pergunta = require('./database/Pergunta');
-// const Resposta = require('./database/Resposta');
-
-//Database
-// connection
-//   .authenticate()
-//   .then(() => {
-//     console.log('DB Conectado!');
-//   })
-//   .catch((msgErro) => {
-//     console.log(msgErro);
-//   })
 
 //Estou dizendo para o Express usar o EJS como View engine
 app.set('view engine', 'ejs');
@@ -23,63 +10,8 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//Rotas
 app.get('/', (req, res) => {
-  Pergunta.findAll({ raw: true, order: [['id', 'DESC']] }) //ASC = Crescente || DESC = Decrescente
-    .then(perguntas => {
-      console.log(perguntas);
-      res.render('index', {
-        perguntas: perguntas
-      });
-    });
-});
-
-app.get('/perguntar', (req, res) => {
   res.render('perguntar');
-});
-
-app.post('/salvarPergunta', (req, res) => {
-  let titulo = req.body.titulo;
-  let descricao = req.body.descricao;
-  Pergunta.create({
-    titulo: titulo,
-    descricao: descricao
-  }).then(() => {
-    res.redirect('/')
-  });
-});
-
-app.get('/pergunta/:id', (req, res) => {
-  let id = req.params.id;
-  Pergunta.findOne({
-    where: { id: id },
-  }).then(pergunta => {
-    if (pergunta != undefined) {
-
-      Resposta.findAll({
-        where: { perguntaId: pergunta.id },
-        order: [['id', 'DESC']]
-      }).then(respostas => {
-        res.render('pergunta', {
-          pergunta: pergunta,
-          respostas: respostas
-        });
-      });
-    } else {
-      res.redirect('/');
-    }
-  });
-});
-
-app.post('/responder', (req, res) => {
-  let corpo = req.body.corpo;
-  let perguntaId = req.body.pergunta;
-  Resposta.create({
-    corpo: corpo,
-    perguntaId: perguntaId
-  }).then(() => {
-    res.redirect('/pergunta/' + perguntaId)
-  });
 });
 
 //Configuração a porta localhost
