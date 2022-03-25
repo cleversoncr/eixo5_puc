1115460fe3c62b60fe9b92f79c6cc824a697e48a
Cleverson Rocha <cleverson.rocha@minu.co>
2022-03-24 22:03:07 -0300

    Correcao da estrutura de pastas

--- s05_projeto-1/database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const connection = new Sequelize('guiaperguntas', 'sa', '54Gy934f86vX4y2k', {
-host: 'localhost',
-dialect: 'mssql'
-});
-
-module.exports = connection;
